2416ab7b3d4a0bfc2d21e0a0b6da8ad516ce8253
Rich Mehta <rich@factorypattern.co.uk>
2013-06-18 09:06:55 +0100

    Stubbed a paypal payment gateway out

---
+++ gateways/class.fp-paypal.php
@@ -0,0 +1,11 @@
+<?php
+	require_once( plugin_dir_url( __FILE__ ).'libs/paypal-digital-goods/paypal-digital-goods.class.php' );
+	
+	class FP_PayPal {
+		
+		function __construct() {
+			
+		}
+		
+	}
+?>
