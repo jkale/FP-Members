9516d13df7558e713554f98146d2e2f344de3b83
Rich Mehta <rich@factorypattern.co.uk>
2013-10-09 12:46:45 +0100

    Updated file location

--- classes/class.role-editor.php
+++ classes/class.role-editor.php
@@ -1,6 +1,6 @@
 <?php
 	require_once 'class.roles.php';
-	require_once '../views/helpers/admin/roles/roles.php';
+	require_once '../../views/helpers/admin/roles/roles.php';
 	
 	/**
 	 * Creates the membership levels interface where you can edit all the 
